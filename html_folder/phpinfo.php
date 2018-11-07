e29cafaa3117523d714bde122a7e4daee33c0dc7
Jerry <zoj3zoj3@gmail.com>
2018-11-07 12:22:13 -0800

    this is the testing request

--- html_folder/phpinfo.php
+++ html_folder/phpinfo.php
@@ -1,3 +1,26 @@
 <?php
-  echo phpinfo();
+  if ($_GET) {
+    echo "get request accepted".PHP_EOL;
+    echo "content is ".PHP_EOL;
+    var_dump($_GET["data"]);
+    $json = $_GET["data"];
+
+    echo $json;
+    $parse_json = json_decode($json);
+    var_dump($parse_json);
+    // $array = ["data"=>['name'=>"jerry","email"=>"jerry@hotmail.com"]];
+    // var_dump($array);
+    // $json_array = json_encode($array);
+    // echo $json_array;
+    // $data = json_decode($json);
+    // var_dump($data);
+  }
+  // var_dump($_REQUEST);
+
+  if ($_POST) {
+    echo "post request accepted".PHP_EOL;
+    echo "content is ".PHP_EOL;
+
+
+  }
 ?>
