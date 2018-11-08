7c61166123169431a8004a71990b15b18641c109
Jerry <zoj3zoj3@gmail.com>
2018-11-08 10:32:10 -0800

    make a api folder for api request

--- html_folder/api/sensor_process.php
+++ html_folder/api/sensor_process.php
@@ -6,8 +6,8 @@ require "../global_config/development_data_process/db_helper.inc";
 
   if ($_GET) {
     $db = new db_helper();
-    echo "get request accepted".PHP_EOL;
-    echo "content is ".PHP_EOL;
+    // echo "get request accepted".PHP_EOL;
+    // echo "content is ".PHP_EOL;
     // var_dump($_GET["data"]);
     $json = $_GET["data"];
 
@@ -18,7 +18,9 @@ require "../global_config/development_data_process/db_helper.inc";
     unset($parse_json["product_id"]);
     $parse_json["update_date"] = date("Y-m-d G:i:s",time());
 
-    $db->insert($parse_json,$table_name);
+    $id = $db->insert($parse_json,$table_name);
+
+    echo "save successfully and the id is $id".PHP_EOL;
     // $array = ["data"=>['name'=>"jerry","email"=>"jerry@hotmail.com"]];
     // var_dump($array);
     // $json_array = json_encode($array);
