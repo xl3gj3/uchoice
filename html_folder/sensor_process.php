5dcbefad801ad90bde12eecf786a401fc532286b
Jerry <zoj3zoj3@gmail.com>
2018-11-07 16:52:37 -0800

    GET method test

--- html_folder/sensor_process.php
+++ html_folder/sensor_process.php
@@ -1,13 +1,24 @@
 <?php
+// require "../global_config/development_data_process/test.inc";
+require "../global_config/development_data_process/config.inc";
+require "../global_config/development_data_process/Database.class.inc";
+require "../global_config/development_data_process/db_helper.inc";
+
   if ($_GET) {
+    $db = new db_helper();
     echo "get request accepted".PHP_EOL;
     echo "content is ".PHP_EOL;
-    var_dump($_GET["data"]);
+    // var_dump($_GET["data"]);
     $json = $_GET["data"];
 
-    echo $json;
-    $parse_json = json_decode($json);
+    // echo $json;
+    $parse_json = json_decode($json,true);
     var_dump($parse_json);
+    $table_name = "data_product_".$parse_json["product_id"];
+    unset($parse_json["product_id"]);
+    $parse_json["update_date"] = date("Y-m-d G:i:s",time());
+
+    $db->insert($parse_json,$table_name);
     // $array = ["data"=>['name'=>"jerry","email"=>"jerry@hotmail.com"]];
     // var_dump($array);
     // $json_array = json_encode($array);
