b2e0e55ebbcae6315f02ea64ce1eecf1ab936c15
Jerry <zoj3zoj3@gmail.com>
2018-11-13 11:17:44 -0800

    change the global folder name

--- html_folder/api/sensor_process.php
+++ html_folder/api/sensor_process.php
@@ -1,8 +1,8 @@
 <?php
 // require "../global_config/development_data_process/test.inc";
-require "../../global_config/development_data_process/config.inc";
-require "../../global_config/development_data_process/Database.class.inc";
-require "../../global_config/development_data_process/db_helper.inc";
+require "../../global_includes/development_data_process/config.inc";
+require "../../global_includes/development_data_process/Database.class.inc";
+require "../../global_includes/development_data_process/db_helper.inc";
 
   if ($_GET) {
     $db = new db_helper();
@@ -11,16 +11,16 @@ require "../../global_config/development_data_process/db_helper.inc";
     // var_dump($_GET["data"]);
     $json = $_GET["data"];
 
-    // echo $json;
+    echo $json;
     $parse_json = json_decode($json,true);
-    var_dump($parse_json);
+    // var_dump($parse_json);
     $table_name = "data_product_".$parse_json["product_id"];
     unset($parse_json["product_id"]);
     $parse_json["update_date"] = date("Y-m-d G:i:s",time());
 
     $id = $db->insert($parse_json,$table_name);
 
-    echo "save successfully and the id is $id".PHP_EOL;
+    // echo "save successfully and the id is $id".PHP_EOL;
     // $array = ["data"=>['name'=>"jerry","email"=>"jerry@hotmail.com"]];
     // var_dump($array);
     // $json_array = json_encode($array);
@@ -30,10 +30,4 @@ require "../../global_config/development_data_process/db_helper.inc";
   }
   // var_dump($_REQUEST);
 
-  if ($_POST) {
-    echo "post request accepted".PHP_EOL;
-    echo "content is ".PHP_EOL;
-
-
-  }
 ?>
