0c452546400104e1464c39683410b532c51e1fa0
Jerry <zoj3zoj3@gmail.com>
2018-12-04 16:32:39 -0800

    change the msg structure

--- html_folder/api/sensor_process.php
+++ html_folder/api/sensor_process.php
@@ -11,12 +11,15 @@
   $dic_lenght = array(
     "id_len" => 8,
     "nubmer_sensors_len" => 2,
-    "name_len" => 10,
+    "msgTypeLengh"=>2,
+    "name_len" => 12,
     "data_type_len" => 2
+  );
+  $msgType = array(
+    "D" => "data"
   );
   $data_structure = array(
     "serial_id" => "",
-    "customer_id" => 0,
     "sensor_type" => "",
     "data" => 0,
     "update_date" =>"",
@@ -34,30 +37,37 @@
     $data = $_GET["data"];
     $output = $tranform->parse_data($data);
     // echo "after parse".PHP_EOL;
-    // var_dump($output);
-
-    $number_of_sensor = $tranform->get_number_sensors();
-    // echo "number of the sensor $number_of_sensor".PHP_EOL;
-    $organization = new organizing_data($output);
-    $output = $organization->oraganized_data();
+    var_dump($output);
     // echo "after return from organization".PHP_EOL;
-    // var_dump($output);
-    $arr_key = array_keys($output);
-    // var_dump($arr_key);
-    for ($i=0; $i < sizeof($arr_key); $i++) {
-      if (array_key_exists($arr_key[$i],$data_structure)) {
-        $data_structure[$arr_key[$i]] = $output[$arr_key[$i]];
-        unset($output[$arr_key[$i]]);
+
+    if ($msgType[$output["msgType"]]) {
+      unset($output["msgType"]);
+      $number_of_sensor = $tranform->get_number_sensors();
+
+      $organization = new organizing_data($output);
+      $output = $organization->oraganized_data();
+      var_dump($output);
+
+      $arr_key = array_keys($output);
+      for ($i=0; $i < sizeof($arr_key); $i++) {
+        if (array_key_exists($arr_key[$i],$data_structure)) {
+          $data_structure[$arr_key[$i]] = $output[$arr_key[$i]];
+          unset($output[$arr_key[$i]]);
+        }
       }
-    }
-    $sensor_name = array_keys($output);
-    for ($i=0; $i < $number_of_sensor; $i++) {
-      $data_structure["sensor_type"] = $sensor_name[$i];
-      $data_structure["data"] = $output[$sensor_name[$i]];
+      $sensor_name = array_keys($output);
 
-      $id = $db->insert($data_structure, $table_name);
+      for ($i=0; $i < $number_of_sensor; $i++) {
+        $data_structure["sensor_type"] = $sensor_name[$i];
+        $data_structure["data"] = $output[$sensor_name[$i]];
 
-      echo "successed insert and id is $id" .PHP_EOL;
+        // $status = $this->db->insert($this->data_structure, $this->table_name);
+        $status = $db->insert($data_structure, $table_name);
+
+      }
+      if ($status) {
+        echo "Insert Done";
+      }
     }
   }
 
